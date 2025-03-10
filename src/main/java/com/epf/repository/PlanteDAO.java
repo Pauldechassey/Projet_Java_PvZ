f5d4edb80a3afe4875aae4ab5b0833560ff48ab8
pdechassey <paul.debureteldechasse@epfedu.fr>
2025-03-10 15:14:45 +0100

    Projet Séance 2, dernier commit avant les tests

--- src/main/java/com/epf/repository/PlanteDAO.java
+++ src/main/java/com/epf/repository/PlanteDAO.java
@@ -8,6 +8,7 @@ import com.epf.model.Plante;
 public interface PlanteDAO {
     List<Plante> findAll();
     Optional<Plante> findById(int id);
+    void create(Plante plante);
     void save(Plante plante);
     void update(Plante plante);
     void delete(int id);
